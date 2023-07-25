cdaa27b9dbe5f9b32235d9d45f520b5ee07fd894
Yakup <yakupjoraev@gmail.com>
2023-07-25 20:55:09 +0300

    added full about page

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,19 @@
 // Custom scripts
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".main-about__thanks-slider", {
-    slidesPerView: 3,
-    spaceBetween: 32,
-    navigation: {
-      nextEl: ".main-about__thanks-next",
-      prevEl: ".main-about__thanks-prev",
-    },
-  });
+var swiper1 = new Swiper(".main-about__thanks-slider", {
+  slidesPerView: "auto",
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".main-about__thanks-next",
+    prevEl: ".main-about__thanks-prev",
+  },
+});
+
+
+var swiper2 = new Swiper(".main-about__revievs-slider", {
+  slidesPerView: "auto",
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".main-about__revievs-next",
+    prevEl: ".main-about__revievs-prev",
+  },
 });
