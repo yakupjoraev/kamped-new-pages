a6bc5e7045cad76f1e1253acd66756b79380d060
Yakup <yakupjoraev@gmail.com>
2023-07-27 09:26:34 +0300

    added new blocks

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,43 @@ var swiper2 = new Swiper(".main-about__revievs-slider", {
     prevEl: ".main-about__revievs-prev",
   },
 });
+
+var swiper3 = new Swiper(".main-page__info-inner", {
+  slidesPerView: 1.2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".main-page__info-arrow-next",
+    prevEl: ".main-page__info-arrow-prev",
+  },
+
+  breakpoints: {
+
+    // when window width is >= 767
+    525: {
+      slidesPerView: 2.2,
+      spaceBetween: 20
+    },
+
+    // when window width is >= 767
+    767: {
+      slidesPerView: 2.2,
+      spaceBetween: 32
+    },
+
+    // when window width is >= 992
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 32
+    }
+  }
+});
+
+
+var swiper4 = new Swiper(".main-page__accessibility-slider", {
+  slidesPerView: 1,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".main-page__accessibility-arrow-next",
+    prevEl: ".main-page__accessibility-arrow-prev",
+  },
+});
